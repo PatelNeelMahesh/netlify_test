65894a207fc87e6a3d8f8925cdf1c7e7c7afa28f
unknown <pneel361@gmail.com>
2023-12-22 22:53:46 +0530

    added react sorce to deploy without node_modules and dist

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// styled components
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Name</h1>
+      <p className='name'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae aut minima obcaecati veniam aliquid qui expedita harum, incidunt neque veritatis.</p>
+    </div>
+  )
+}
+
+export default App;
+
+// 
