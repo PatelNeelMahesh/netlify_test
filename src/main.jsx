b1341aa221da690ef1e5f292d0f72d38cef66ba6
unknown <pneel361@gmail.com>
2023-12-22 23:02:52 +0530

    uncommented MUI code

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import MUI from './MUI.jsx'
+import MUI from './MUI.jsx'
 
 ReactDOM.createRoot(document.getElementById('neel')).render(
   <React.StrictMode>
     <App />
-    {/* <MUI /> */}
+    <MUI />
   </React.StrictMode>,
 )
